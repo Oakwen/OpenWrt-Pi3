eeade065b1de663fac2a0ef310bca378d4051e02
Oakwen <dai.tianwen@gmail.com>
2020-10-13 13:56:34 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -5,20 +5,22 @@
 
 mkdir -p package/custom
 cd package/custom
-# git clone https://github.com/coolsnowwolf/packages
-git clone https://github.com/kenzok8/small
-git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/Lienol/openwrt-package
-git clone https://github.com/fw876/helloworld
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-git clone https://github.com/vernesong/OpenClash
 git clone https://github.com/tty228/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
 git clone -b lede https://github.com/pymumu/luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns
 git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/vernesong/OpenClash
+git clone https://github.com/Lienol/openwrt-package
+git clone https://github.com/fw876/helloworld
+git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/kenzok8/small
 
 cd ../../
+
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 # Modify default IP
 
