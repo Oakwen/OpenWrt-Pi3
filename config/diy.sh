02e0ed34a752a40d929c41bf64737c0a71cd5719
Oakwen <dai.tianwen@gmail.com>
2022-11-24 20:15:29 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 # 本脚本工作目录必须是git仓库的主目录
-
 # Add Some Package
-
-mkdir -p package/custom
-cd package/custom
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-git clone https://github.com/tty228/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone -b lede https://github.com/pymumu/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns
-git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
-git clone https://github.com/vernesong/OpenClash
-git clone https://github.com/Lienol/openwrt-package
-git clone https://github.com/fw876/helloworld
-git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/kenzok8/small
-
-cd ../../
-
 ./scripts/feeds update -a
+
+rm -rf package/lean/luci-app-jd-dailybonus package/lean/luci-theme-argon
+rm -rf package/feeds/packages/smartdns
+mkdir -p package/custom && cd package/custom
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns.git
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git
+git clone --depth=1 https://github.com/pymumu/smartdns.git
+git clone --depth=1 https://github.com/vernesong/OpenClash.git
+git clone --depth=1 https://github.com/fw876/helloworld.git
+
+cd -
 ./scripts/feeds install -a
 
 # Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
-sed -i 's/192.168.1.1/192.168.8.100/g' package/base-files/files/bin/config_generate
-# sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
-
-# Add some default settings
-
-#\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
-curl -fsSL https://raw.githubusercontent.com/Oakwen/Openwrt/master/config/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
+# Modify some default settings
+# curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lede/zzz-default-settings >package/lean/default-settings/files/zzz-default-settings
